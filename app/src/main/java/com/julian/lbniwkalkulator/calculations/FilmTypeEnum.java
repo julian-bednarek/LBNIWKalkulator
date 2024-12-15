7a0710c7cbac361ad117428d5d11103bb7a87044
Julian Bednarek <juliuszxxbednarek@gmail.com>
2024-12-15 15:40:38 +0100

    Filled FilmTypeEnum with proper data

--- app/src/main/java/com/julian/lbniwkalkulator/calculations/FilmTypeEnum.java
+++ app/src/main/java/com/julian/lbniwkalkulator/calculations/FilmTypeEnum.java
@@ -1,4 +1,26 @@
 package com.julian.lbniwkalkulator.calculations;
 
 public enum FilmTypeEnum {
+    _D1(1),
+    _D2(2),
+    _D3(3),
+    _D4(4),
+    _D5(5),
+    _D6(6),
+    _D7(7);
+
+
+    private int typeNumber;
+
+    FilmTypeEnum(int typeNumber) {
+        this.typeNumber = typeNumber;
+    }
+
+    public int getTypeNumber() {
+        return typeNumber;
+    }
+
+    public void setTypeNumber(int typeNumber) {
+        this.typeNumber = typeNumber;
+    }
 }
